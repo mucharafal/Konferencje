019e99a7e6ad2e0e7712b601cfc1f6ac8a9c5a27
Marcin Pham <marcin.kamo@gmail.com>
2018-01-21 17:33:38 +0100

    Dodano usunięcie Company

---
+++ procedures/RemoveCompany.sql
@@ -0,0 +1,23 @@
+create procedure RemoveCompany (
+    @CompanyID int
+) as
+declare @AddressID as int
+begin transaction
+    begin try
+        select @AddressID = AddressID from Companies
+        left join CompanyReservations as cr 
+        on cr.CompanyID = Companies.CompanyID 
+        where cr.CompanyID = null
+        and @CompanyID = Companies.CompanyID 
+        if @CompanyID <> null
+            delete from Companies
+            where @CompanyID = CompanyID
+            exec RemoveAddress(@AddressID);
+    begin catch
+        if @@TRANCOUNT > 0 rollback transaction;
+		declare @errorMsg nvarchar(2048)
+			= 'Cannot remove Company. Error message: ' + ERROR_MESSAGE();
+		;Throw 52000, @errorMsg, 1
+	end catch
+commit transaction
+        
