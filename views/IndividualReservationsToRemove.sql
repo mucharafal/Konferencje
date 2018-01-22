78eef1b36a08c4771470d85cd9773835001d0afe
Marcin Pham <marcin.kamo@gmail.com>
2018-01-22 10:41:51 +0100

    Dodano widok do usuwania nieopłaconych rezerwacji prywatnych

---
+++ views/IndividualReservationsToRemove.sql
@@ -0,0 +1,6 @@
+create view IndividualUserReservations as
+select UserID, ur.ReservationID, pr.Date, pfr.Price, pr.Paid, ConferenceName, NumOfEdition, ConferenceDayDate, WorkshopName from UserReservations as ur
+inner join PrivateReservations as pr on pr.ReservationID = ur.ReservationID
+inner join PriceForReservation as pfr on pfr.ReservationID = ur.ReservationID
+where pr.Paid = 0
+and dateadd(day, 7, pr.Date) > getdate()
