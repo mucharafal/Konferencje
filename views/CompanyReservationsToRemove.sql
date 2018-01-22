9b6989c37f55c4a733b3eb3925f71f73a4f079dd
Marcin Pham <marcin.kamo@gmail.com>
2018-01-22 10:37:25 +0100

    Dodano widok dla triggera usuwania nieopłaconych rezerwacji firmowych

---
+++ views/CompanyReservationsToRemove.sql
@@ -0,0 +1,7 @@
+create view CompanyReservationsToRemove as 
+select cr.CompanyReservationID, c.ConferenceName from CompanyReservations as cr
+inner join ConferenceEditions as ce on ce.ConferenceEditionID = cr.ConferenceEditionID
+inner join Conferences as c on ce.ConferenceID = c.ConferenceID
+inner join PriceForCompanyReservation as prices on prices.CompanyReservationID = cr.CompanyReservationID
+where cr.Paid = 0 
+and DATEADD(day, 14, getdate()) > cr.Date
