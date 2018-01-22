91104540f4e5b3e2694efa8035d994e27e235c74
Marcin Pham <marcin.kamo@gmail.com>
2018-01-22 10:56:51 +0100

    naprawiono nazwę

--- views/IndividualReservationsToRemove.sql
+++ views/IndividualReservationsToRemove.sql
@@ -1,4 +1,4 @@
-create view IndividualUserReservations as
+create view IndividualReservationsToRemove as
 select UserID, ur.ReservationID, pr.Date, pfr.Price, pr.Paid, ConferenceName, NumOfEdition, ConferenceDayDate, WorkshopName from UserReservations as ur
 inner join PrivateReservations as pr on pr.ReservationID = ur.ReservationID
 inner join PriceForReservation as pfr on pfr.ReservationID = ur.ReservationID
